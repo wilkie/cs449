64e95bace9849cb4046460f9b1bf925e00c6c795
wilkie <wilkie05@gmail.com>
2020-02-10 02:42:32 -0500

    Reverts abs example.

--- examples/data-representation/abs.c
+++ examples/data-representation/abs.c
@@ -2,15 +2,15 @@
 #include <limits.h>   /* INT_MIN, etc */
 
 /* Returns the absolute value of the given integer. */
-int abs(int x, int y) {
+int abs(int x) {
   if (x < 0) {
     x = -x;
   }
 
-  return y;
+  return x;
 }
 
 int main() {
-  printf("|%d| = %d\n", -5, abs(-5, 3));
-  printf("|%d| = %d\n", INT_MIN, abs(INT_MIN, INT_MIN));
+  printf("|%d| = %d\n", -5, abs(-5));
+  printf("|%d| = %d\n", INT_MIN, abs(INT_MIN));
 }
